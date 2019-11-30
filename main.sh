7c331acd6fe5ce804593912f39e2ab9def598721
H4ku0ok4m1 <edsson.cm@hotmail.com>
2019-11-30 15:36:34 -0600

    Uploaded main script and needed lst file
    Files main.sh and githubtools.lst were uploaded, skipping the optional "gitskip.lst" file that stores

---
+++ main.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+banner () {
+	echo "==============================================================="
+	echo
+	echo "      Common hacking tools download, install and update"
+	echo 
+	echo 
+	echo 
+	echo "    Special thanks to everyone who actually made the tools"
+	echo 
+	echo "==============================================================="
+}
+
+function githubdownload {
+	echo
+	if [ -f githubtools.lst ]; then
+		for gitrepo in `cat githubtools.lst`
+		do
+			repo_name=`sed 's|https://github.com/.*/||' <<< $gitrepo`
+			sanitized=`sed 's|\.git||' <<< $repo_name`
+			
+			if [ -f gitskip.lst ]; then
+				for skipped in `cat gitskip.lst`
+				do
+					if [ $skipped == $sanitized ]; then
+						echo
+						echo "You dont want $sanitized updated/downloaded! I'll skip it!"
+					else
+						download_upgrade
+					fi
+				done
+			else
+				download_upgrade
+			fi
+		done
+	else
+		echo
+		echo "Can't find the tools list! Can't download!"
+		echo "Make sure the file githubtools.lst is in the same working direftory as this script!"
+	fi
+}
+
+function download_upgrade {
+        if [ -d "/opt/$sanitized" ]; then
+                echo
+                echo "Updating $sanitized ..."
+                cd /opt/$sanitized
+                git pull
+        else
+                echo
+                echo "Downloading $sanitized .."
+                git -C /opt/ clone $gitrepo
+        fi
+}
+
+main () {
+	banner
+	echo
+	echo "I will attempt to download/update the tools from github, this might take a while..."
+	githubdownload
+	echo
+	echo "Happy hunting!"
+}
+
+main
