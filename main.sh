dfc26ac45d304fb177219bb32ca77ea7cead0a77
H4ku0ok4m1 <edsson.cm@hotmail.com>
2020-05-04 20:03:40 -0500

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
 banner () {
-	echo "==============================================================="
-	echo
-	echo "      Common hacking tools download, install and update"
-	echo 
-	echo 
-	echo 
-	echo "    Special thanks to everyone who actually made the tools"
-	echo 
-	echo "==============================================================="
+        echo "==============================================================="
+        echo
+        echo "      Common hacking tools download, install and update"
+        echo 
+        echo 
+        echo 
+        echo "    Special thanks to everyone who actually made the tools"
+        echo 
+        echo "==============================================================="
 }
 
 function githubdownload {
-	echo
+        echo
 	if [ -f githubtools.lst ]; then
-		for gitrepo in `cat githubtools.lst`
-		do
-			repo_name=`sed 's|https://github.com/.*/||' <<< $gitrepo`
-			sanitized=`sed 's|\.git||' <<< $repo_name`
-			
-			if [ -f gitskip.lst ]; then
-				for skipped in `cat gitskip.lst`
-				do
-					if [ $skipped == $sanitized ]; then
-						echo
-						echo "You dont want $sanitized updated/downloaded! I'll skip it!"
-					else
-						download_upgrade
-					fi
-				done
-			else
+		if [ -f gitskip.lst ]; then
+			echo "The blacklist file is found, partial download/update."
+			for GITREPO in `cat githubtools.lst`
+			do
+				REPO_NAME=`sed 's|https://git.*.com/.*/||' <<< $GITREPO`
+				SANITIZED=`sed 's|\.git||' <<< $REPO_NAME`
+                                if grep -Fxq "$SANITIZED" $SCRIPTPATH/gitskip.lst; then
+                                        echo "$SANITIZED is in the list, won't do anything"
+                                else
+                                        download_upgrade
+                                fi
+			done
+		else
+			echo "No gitskip.lst file found, downloading/updating everything in the list."
+			for GITREPO in `cat githubtools.lst`
+			do
+				REPO_NAME=`sed 's|https://github.com/.*/||' <<< $GITREPO`
+				SANITIZED=`sed 's|\.git||' <<< $REPO_NAME`
 				download_upgrade
-			fi
-		done
+			done
+		fi
 	else
 		echo
 		echo "Can't find the tools list! Can't download!"
@@ -42,25 +44,26 @@ function githubdownload {
 }
 
 function download_upgrade {
-        if [ -d "/opt/$sanitized" ]; then
+        if [ -d "/opt/$SANITIZED" ]; then
                 echo
-                echo "Updating $sanitized ..."
-                cd /opt/$sanitized
+                echo "Updating $SANITIZED ..."
+                cd /opt/$SANITIZED
                 git pull
         else
                 echo
-                echo "Downloading $sanitized .."
-                git -C /opt/ clone $gitrepo
+                echo "Downloading $SANITIZED .."
+                git -C /opt/ clone $GITREPO
         fi
 }
 
 main () {
-	banner
-	echo
-	echo "I will attempt to download/update the tools from github, this might take a while..."
-	githubdownload
-	echo
-	echo "Happy hunting!"
+        SCRIPTPATH=`pwd`
+        banner
+        echo
+        echo "I will attempt to download/update the tools from github, this might take a while..."
+        githubdownload
+        echo
+        echo "Happy hunting!"
 }
 
 main
